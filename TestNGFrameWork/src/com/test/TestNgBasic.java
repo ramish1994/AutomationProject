be7aea70a7fbb691db0684663c9700b59aec0b56
rmajeed <rmajeed@TRA-UI21.mshome.net>
2022-01-12 12:15:57 +0500

    First commit

--- TestNGFrameWork/src/com/test/TestNgBasic.java
+++ TestNGFrameWork/src/com/test/TestNgBasic.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 public class TestNgBasic {
@@ -18,36 +20,30 @@ public class TestNgBasic {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("https://securehealthcms.qa.greenlightlabs.tech/sitecore/shell/Applications/Content%20Editor.aspx?sc_bw=1");
+		driver.get("https://justdelivery.it/index.php/admin_007/admin/index/index/key/fdde49313e7a27b837725e04c92ab5622292f578fc2359cb969aca40b9f49c35/");
 	
 	}
 	
-	@Test
-	public void checkTitle() {
-		System.out.println("Check title");
-		String title = driver.getTitle();
-		System.out.println(title);
-	}
 
-	@Test
+	@Test(priority = 1, groups = "Login")
 	public void loginAdmin() throws InterruptedException {
-		driver.findElement(By.xpath("//*[@id=\"UserName\"]")).sendKeys("Admin");
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("admin.justdelivery");
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("b");
+		driver.findElement(By.xpath("//*[@id=\"login\"]")).sendKeys("oC9cU#EJ1UcSZ$pA");
 
-		driver.findElement(By.xpath("//*[@id=\"LogInBtn\"]")).click();
-		Thread.sleep(1000);
-						
-		driver.findElement(By.xpath("/html/body/div[1]/div/section/section/div/div/div[1]/div[2]/div[1]/a[1]/span[1]")).click();
-		Thread.sleep(1000);
-		//driver.findElement(By.xpath("//a//span[contains(text(),'Home')]")).click();
-		//Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[text()='Title - Please enter title of the item here:']/..//input")).sendKeys("Ramish");
-		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div[3]/div[1]/button")).click();
+		Thread.sleep(1000);				
+
 	}
-	@AfterMethod
-	public void tearDown() {
-		driver.quit();
+	@Test(priority = 2, groups ="Login")
+	public void openMenu() throws InterruptedException {
+		driver.findElement(By.xpath("//*[@id=\"menu-magento-customer-customer\"]/a")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//*[@id=\"menu-magento-customer-customer\"]/div/ul/li[1]/a")).click();
+	}
+	@AfterClass
+	public void closeBrowser() {
+		
 	}
 
 	
